e6f31a09d96fc7f775a73f17121db48cf828de11
JoaoVitor1021 <joao.moura21@escola.pr.gov.br>
2025-02-17 08:34:52 -0300

    Add files via upload

---
+++ atividade1.js
@@ -0,0 +1,34 @@
+ console.log("atividade 1")
+ console.log("")
+ const joao = "joao"
+ console.log(joao)
+
+ console.log("")
+
+ console.log("atividade 2")
+ console.log("")
+ const num1 = 3
+ const num2 = 4
+ 
+const soma = num1+num2
+console.log(soma)
+
+console.log("")
+
+console.log("atividade 3")
+
+const frase1 = ("bom ")
+const frase2 = ("dia")
+const junçao = frase1+frase2
+console.log(junçao)
+
+console.log("")
+
+console.log("atividade 4")
+
+const n1 = 6
+const n2 = 7
+const n3 = 8
+const media = (n1+n2+n3)/3
+
+console.log(media)
